16e90eecdcf6d1bf1b7f89e539cd3e084c1e2675
Giacomo Garufi <119006946+overpoi@users.noreply.github.com>
2025-05-08 13:20:19 +0100

    Update project4.js

--- Project3_TriangularMeshes/project4.js
+++ Project3_TriangularMeshes/project4.js
@@ -171,6 +171,8 @@ class MeshDrawer
 
 		// Set texture parameters 
 
+		this.showTexture(true);
+		
 		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR );
 		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR );
 		gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT );
